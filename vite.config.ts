88776a9de807d122bbf22cdf2bf08b86b04fc688
Joaquin Senosiain <jsenosia@BURLMT2N57L0W4.local>
2025-02-07 14:27:00 -0800

    Initial Commit

---
+++ vite.config.ts
@@ -0,0 +1,57 @@
+/// <reference types="vite/client" />
+/// <reference types="vitest" />
+import { globSync } from 'glob'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path, { resolve } from 'path'
+import dts from 'vite-plugin-dts'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
+import { fileURLToPath } from 'url'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      input: Object.fromEntries(
+        globSync(['src/components/**/index.tsx', 'src/main.ts']).map((file) => {
+          // This remove `src/` as well as the file extension from each
+          // file, so e.g. src/nested/foo.js becomes nested/foo
+          const entryName = path.relative(
+            'src',
+            file.slice(0, file.length - path.extname(file).length)
+          )
+          // This expands the relative paths to absolute paths, so e.g.
+          // src/nested/foo becomes /project/src/nested/foo.js
+          const entryUrl = fileURLToPath(new URL(file, import.meta.url))
+          return [entryName, entryUrl]
+        })
+      ),
+      output: {
+        entryFileNames: '[name].js',
+        assetFileNames: 'assets/[name][extname]',
+        globals: {
+          react: 'React',
+          'react-dom': 'React-dom',
+          'react/jsx-runtime': 'react/jsx-runtime',
+        },
+      },
+    },
+  },
+  plugins: [react(), libInjectCss(), dts({ exclude: ["**/*.stories.tsx", "src/test", "**/*.test.tsx"] })],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    // you might want to disable it, if you don't have tests that rely on CSS
+    // since parsing CSS is slow
+    css: true,
+    coverage: {
+      include: ["src/components"],
+      exclude: ["**/*.stories.tsx"],
+    },
+  },
+})
